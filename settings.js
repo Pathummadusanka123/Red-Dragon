c3b99b2774fba7d20f6731701db47ab436c2e0be
Pathummadusanka123 <108622025+Pathummadusanka123@users.noreply.github.com>
2022-07-06 06:56:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,12 +19,12 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
+global.alivemsg = `My name is pathum⦁⦁❱🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94760601107'] //Owner number in aive msg
+global.premium = ['+94760601107'] //Owner Number info
+global.ownernomer = '+0760390292' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
